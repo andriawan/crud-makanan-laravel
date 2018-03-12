d0fdbb6f6088b3ba230768948e465dcc6b46b17a
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2018-03-12 12:02:29 +0700

    improve view layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,9 @@
 </head>
 <body>
 
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#" style="color: black;">Simple Makanan CRUD</a>
+</nav>
 
 <div class="container">
     @yield('content')
